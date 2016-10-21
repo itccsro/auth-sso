9f15ef5131ceb4ad0f661f42852857cc10ba8703
alexchiraples <alexandru.chiraples@ithub.gov.ro>
2016-10-21 13:15:17 +0300

    Revert "Revert "- added sources""
    This reverts commit a83b9b22aa4384d8b11bc5c436ef24517989d846.

---
+++ src/GovITHub.Auth.Identity/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GovITHub.Auth.Identity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls(args)
+                .Build();
+
+            host.Run();
+        }
+    }
+}
