478a0ff70619de7ad98bbafea0e9e076d381ac73
dmihaita <daniel.mihaita@gmail.com>
2016-10-25 22:33:20 +0300

    Issue #18.#19 - store Scopes and Clients to DB

--- src/GovITHub.Auth.Identity/Startup.cs
+++ src/GovITHub.Auth.Identity/Startup.cs
@@ -13,6 +13,10 @@ using GovITHub.Auth.Identity.Data;
 using GovITHub.Auth.Identity.Models;
 using GovITHub.Auth.Identity.Services;
 using MySQL.Data.Entity.Extensions;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using System.Reflection;
+using System.Diagnostics;
 //using MySQL.Data.EntityFrameworkCore.Extensions;
 
 namespace GovITHub.Auth.Identity
@@ -42,10 +46,11 @@ namespace GovITHub.Auth.Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string mySqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySQL(Configuration
-                    .GetConnectionString("DefaultConnection")));
+                options.UseMySQL(mySqlConnectionString));
                 //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
@@ -70,8 +75,12 @@ namespace GovITHub.Auth.Identity
 
             // Adds IdentityServer
             services.AddDeveloperIdentityServer()
-                .AddInMemoryScopes(Config.GetScopes())
-                .AddInMemoryClients(Config.GetClients())
+                .AddConfigurationStore(builder => 
+                    builder.UseMySQL(mySqlConnectionString, 
+                        options => options.MigrationsAssembly(migrationsAssembly)))
+                .AddOperationalStore(builder =>
+                    builder.UseMySQL(mySqlConnectionString, options =>
+                        options.MigrationsAssembly(migrationsAssembly)))
                 .AddAspNetIdentity<ApplicationUser>();
         }
 
@@ -80,6 +89,16 @@ namespace GovITHub.Auth.Identity
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            try
+            {
+                InitializeDatabase(app);
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Startup>();
+                logger.LogCritical("Error initializing database. Reason : {0}", ex);
+            }
 
             app.UseCors("CorsPolicy");
 
@@ -109,6 +128,41 @@ namespace GovITHub.Auth.Identity
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            try
+            {
+                using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+                {
+                    scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                    var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                    context.Database.Migrate();
+                    if (context.Clients.FirstOrDefault() == null)
+                    {
+                        foreach (var client in Config.GetClients())
+                        {
+                            context.Clients.Add(client.ToEntity());
+                        }
+                        context.SaveChanges();
+                    }
+
+                    if (context.Scopes.FirstOrDefault() == null)
+                    {
+                        foreach (var client in Config.GetScopes())
+                        {
+                            context.Scopes.Add(client.ToEntity());
+                        }
+                        context.SaveChanges();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Trace.TraceError("Error : {0}", ex);
+            }
         }
     }
 }
