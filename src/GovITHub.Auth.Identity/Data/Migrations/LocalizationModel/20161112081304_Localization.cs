6cc503288bc3b6b1f1a12e36d348e14c1d1713bf
dmihaita <daniel.mihaita@gmail.com>
2016-11-12 10:49:35 +0200

    Integrated Damien Bod's sql localization implementation
    Conflicts:
    	src/GovITHub.Auth.Identity/project.json

---
+++ src/GovITHub.Auth.Identity/Data/Migrations/LocalizationModel/20161112081304_Localization.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovITHub.Auth.Identity.Data.Migrations.LocalizationModel
+{
+    public partial class Localization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExportHistory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Exported = table.Column<DateTime>(nullable: false),
+                    Reason = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExportHistory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImportHistory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Imported = table.Column<DateTime>(nullable: false),
+                    Information = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImportHistory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocalizationRecord",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Key = table.Column<string>(nullable: false),
+                    LocalizationCulture = table.Column<string>(nullable: false),
+                    ResourceKey = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    UpdatedTimestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalizationRecord", x => x.Id);
+                    table.UniqueConstraint("AK_LocalizationRecord_Key_LocalizationCulture", x => new { x.Key, x.LocalizationCulture });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExportHistory");
+
+            migrationBuilder.DropTable(
+                name: "ImportHistory");
+
+            migrationBuilder.DropTable(
+                name: "LocalizationRecord");
+        }
+    }
+}
---
+++ src/GovITHub.Auth.Identity/Infrastructure/Configuration/LocalizationConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Localization.SqlLocalizer.DbStringLocalizer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using MySQL.Data.Entity.Extensions;
+using System.Collections.Generic;
+using System.Globalization;
+using WebApiContrib.Core.Formatter.Csv;
+
+namespace GovITHub.Auth.Identity.Infrastructure.Configuration
+{
+    public static class LocalizationConfiguration
+    {
+        public static IServiceCollection ConfigureLocalization(this IServiceCollection services, 
+            string connectionString, string migrationsAssembly)
+        {
+            services.AddDbContext<LocalizationModelContext>(options => 
+                options.UseMySQL(connectionString , mo => mo.MigrationsAssembly(migrationsAssembly)));
+            // Requires that LocalizationModelContext is defined
+            services.AddSqlLocalization(options => options.UseTypeFullNames = true);
+
+            services.AddMvc()
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                {
+                    var supportedCultures = new List<CultureInfo> {
+                        new CultureInfo("ro-RO"),
+                        new CultureInfo("en-US")
+                    };
+
+                    options.DefaultRequestCulture = new RequestCulture(culture: "ro-RO", uiCulture: "ro-RO");
+                    options.SupportedCultures = supportedCultures;
+                    options.SupportedUICultures = supportedCultures;
+                }
+            );
+
+            var csvFormatterOptions = new CsvFormatterOptions();
+
+            services.AddMvc(options =>
+            {
+                options.InputFormatters.Add(new CsvInputFormatter(csvFormatterOptions));
+                options.OutputFormatters.Add(new CsvOutputFormatter(csvFormatterOptions));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("csv", MediaTypeHeaderValue.Parse("text/csv"));
+            });
+
+            // services.AddScoped<ValidateMimeMultipartContentFilter>();
+            return services;
+        }
+    }
+}
