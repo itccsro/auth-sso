b936b0ce031ca7490719914c28c66382859965d1
dmihaita <daniel.mihaita@gmail.com>
2016-12-12 18:49:43 +0200

    Syntax changes (code review)

--- src/GovITHub.Auth.Admin/Controllers/Api/UserIdentityController.cs
+++ src/GovITHub.Auth.Admin/Controllers/Api/UserIdentityController.cs
@@ -5,32 +5,25 @@ using IdentityModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace GovITHub.Auth.Admin.Controllers.Api
 {
     [Authorize]
     [Route("api/[controller]")]
     public class UserIdentityController : Controller
     {
-        IOrganizationRepository _organizationRepository;
+        private readonly IOrganizationRepository _organizationRepository;
         public UserIdentityController(IOrganizationRepository organizationRepository)
         {
             _organizationRepository = organizationRepository;
         }
-        // GET: api/values
+
         [HttpGet]
         public IActionResult Get()
         {
             string userName = User.Claims.GetClaim(JwtClaimTypes.Name);
             var level = User.Claims.GetClaim<OrganizationUserLevel>(JwtClaimTypes.Role);
             long organizationId = User.Claims.GetClaim<long>("OrganizationId");
-            string organizationName = string.Empty;
-            if (organizationId > 0)
-            {
-                var organization = _organizationRepository.Find(organizationId);
-                organizationName = organization?.Name;
-            }
+            string organizationName = _organizationRepository.Find(organizationId)?.Name;
             return Json(new { username = userName, level = level, organizationId = organizationId, organizationName = organizationName});
         }
     }
