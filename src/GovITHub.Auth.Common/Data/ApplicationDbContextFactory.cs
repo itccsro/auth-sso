a553aa5007bb3ef0222870133820c7565142d133
alexchiraples <alexandru.chiraples@ithub.gov.ro>
2017-08-01 08:52:58 +0300

    update to VS 2017 (xproj to csproj structure) update nuget packages to latest version fix some localization issues TODO: fix localization issue on register

--- src/GovITHub.Auth.Common/Data/ApplicationDbContextFactory.cs
+++ src/GovITHub.Auth.Common/Data/ApplicationDbContextFactory.cs
@@ -1,45 +1,48 @@
-﻿using System.Reflection;
-using GovITHub.Auth.Common.Models;
-using IdentityServer4.EntityFramework.DbContexts;
-using IdentityServer4.EntityFramework.Options;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using MySQL.Data.Entity.Extensions;
-using Localization.SqlLocalizer.DbStringLocalizer;
+﻿//using System.Reflection;
+//using GovITHub.Auth.Common.Models;
+//using IdentityServer4.EntityFramework.DbContexts;
+//using IdentityServer4.EntityFramework.Options;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Infrastructure;
+//using MySQL.Data.Entity.Extensions;
+//using Localization.SqlLocalizer.DbStringLocalizer;
 
-namespace GovITHub.Auth.Common.Data
-{
-    public class ApplicationDbContextFactory : IDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext Create(DbContextFactoryOptions options)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();           
-            optionsBuilder.UseMySQL("DefaultConnection;");
+//namespace GovITHub.Auth.Common.Data
+//{
+//    public class ApplicationDbContextFactory : IDbContextFactory<ApplicationDbContext>
+//    {
+//        public ApplicationDbContext Create(DbContextFactoryOptions options)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();           
+//            optionsBuilder.UseMySQL("DefaultConnection;");
 
-            return new ApplicationDbContext(optionsBuilder.Options);
-        }
-    }
+//            return new ApplicationDbContext(optionsBuilder.Options);
+//        }
+//    }
 
-    public class ConfigurationDbContextFactory : IDbContextFactory<ConfigurationDbContext>
-    {
-        public ConfigurationDbContext Create(DbContextFactoryOptions options)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<ConfigurationDbContext>();
-            var migrationsAssembly = typeof(ApplicationUser).GetTypeInfo().Assembly.GetName().Name;
-            optionsBuilder.UseMySQL("DefaultConnection;", opts => opts.MigrationsAssembly(migrationsAssembly));
+//    public class ConfigurationDbContextFactory : IDbContextFactory<ConfigurationDbContext>
+//    {
+//        public ConfigurationDbContext Create(DbContextFactoryOptions options)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<ConfigurationDbContext>();
+//            var migrationsAssembly = typeof(ApplicationUser).GetTypeInfo().Assembly.GetName().Name;
+//            optionsBuilder.UseMySQL("DefaultConnection;", opts => opts.MigrationsAssembly(migrationsAssembly));
 
-            return new ConfigurationDbContext(optionsBuilder.Options, new ConfigurationStoreOptions());
-        }
-    }
-    public class LocalizationDbContextFactory : IDbContextFactory<LocalizationModelContext>
-    {
-        public LocalizationModelContext Create(DbContextFactoryOptions options)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<LocalizationModelContext>();
-            var migrationsAssembly = typeof(ApplicationUser).GetTypeInfo().Assembly.GetName().Name;
-            optionsBuilder.UseMySQL("DefaultConnection;", opts => opts.MigrationsAssembly(migrationsAssembly));
+//            return new ConfigurationDbContext(optionsBuilder.Options, new ConfigurationStoreOptions());
+//        }
+//    }
+//    public class LocalizationDbContextFactory : IDbContextFactory<LocalizationModelContext>
+//    {
+//        public LocalizationModelContext Create(DbContextFactoryOptions options)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<LocalizationModelContext>();
+//            var migrationsAssembly = typeof(ApplicationUser).GetTypeInfo().Assembly.GetName().Name;
+//            optionsBuilder.UseMySQL("DefaultConnection;", opts => opts.MigrationsAssembly(migrationsAssembly));
 
-            return new LocalizationModelContext(optionsBuilder.Options);
-        }
-    }
-}
+//            SqlContextOptions ctx = new SqlContextOptions();
+//            ctx.SqlSchemaName = "dbo";
+
+//            return new LocalizationModelContext(optionsBuilder.Options);
+//        }
+//    }
+//}
